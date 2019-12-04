923f070180a5d45e065d836b795adc8c3ed3a368
mmcrisostom <cadillacs.x23@gmail.com>
2019-12-04 17:21:49 -0600

    Update v2

--- wapconciliaciondelivery/src/pages/Dashboard.js
+++ wapconciliaciondelivery/src/pages/Dashboard.js
@@ -1,13 +1,32 @@
+import 'date-fns';
+import esLocale from "date-fns/locale/es";
 import React, { useState, useEffect } from 'react';
 import mercadoPago from './../images/general/mercadopago.png';
 import openpay from './../images/general/openpay.png';
 import wow from './../images/general/wow.png';
 import rappi from './../images/general/rappi.png';
 import DownloadExcel from './../components/DownloadExcel';
-import LoadingBlack from './../components/LoadingBlack';
-import boxMenu from './../components/boxMenu';
+import LoadingBlack from './../components/LoadingBlack'
+import api from "../services/ServiceGeneral";
 
-const dataSet = [
+
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+
+import {
+  PAYMENT_METHOD_ID_MERCADOPAGO,
+  PAYMENT_METHOD_ID_WOW,
+  PAYMENT_METHOD_ID_RAPPI,
+  PAYMENT_METHOD_ID_OPENPAY
+} from "../constants/constants"
+
+
+const dataMock = [
   {
     branch: "7097",
     account: "33333333365501945930",
@@ -31,68 +50,82 @@ const dataSet = [
 ];
 
 
-
-
-
 function Dashboard() {
 
   // sate
   const [isLoading, guardarIsLoading] = useState(false);
 
   const [mercadoPagoData, setMercadoPagoData] = useState([]);
-  const [fromDateMP, setFromDateMP] = useState('');
-  const [toDateMP, setToDateMP] = useState('');
+  const [fromDateMP, setFromDateMP] = useState(null);
+  const [toDateMP, setToDateMP] = useState(null);
 
   const [wowData, setWowData] = useState([]);
-  const [fromDateWow, setFromDateWow] = useState('');
-  const [toDateWow, setToDateWow] = useState('');
+  const [fromDateWow, setFromDateWow] = useState(null);
+  const [toDateWow, setToDateWow] = useState(null);
 
   const [openpayData, setOpenpayData] = useState([]);
-  const [fromDateOpenPay, setFromDateOpenPay] = useState('');
-  const [toDateOpenPay, setToDateOpenPay] = useState('');
+  const [fromDateOpenPay, setFromDateOpenPay] = useState(null);
+  const [toDateOpenPay, setToDateOpenPay] = useState(null);
 
   const [rappiData, setRappiData] = useState([]);
-  const [fromDateRappi, setFromDateRappi] = useState('');
-  const [toDateRappi, setToDateRappi] = useState('');
+  const [fromDateRappi, setFromDateRappi] = useState(null);
+  const [toDateRappi, setToDateRappi] = useState(null);
+
+  useEffect(() => {
+    const consultarAPIMP = async () => {
+      try {
+        const data = await api.services.getTokenPayments(1, 2, PAYMENT_METHOD_ID_MERCADOPAGO);
+        console.info(data);
+      } catch (error) {
+
+      }
+    }
+    consultarAPIMP();
+  }, []);
+
+
+  const getPayments = async (fromDate, toDate, paymentId) => {
+    
+    guardarIsLoading(true);
+    let dataPayments = [];
+   
+    try {
+      if (fromDate !== null && toDate !== null) {
+        console.info("001");
+        if (validateTwoDates(fromDate, toDate)) {
+          console.info("002");
+          // const data = await api.services.getPayments(1, 2, paymentId);
+          const data = dataMock;
+          console.info(data);
+
+          return data;
+        }
+      }
+    } catch (error) {
+      dataPayments = [];
+    }
+
+    guardarIsLoading(false);
+    return dataPayments;
+  }
+
 
   // MercadoPago
   useEffect(() => {
     const consultarAPIMP = async () => {
-      if (fromDateMP !== '' && toDateMP !== '') {
-        guardarIsLoading(true);
-        const validDates = validateTwoDates(fromDateMP,toDateMP);
-
-        if(validDates) {
-          setMercadoPagoData(dataSet);
-        } else {
-          setMercadoPagoData([]);
-        }
-
-        guardarIsLoading(false);
-      } else {
-        setMercadoPagoData([]);
-      }
+      const data = await getPayments(fromDateMP, toDateMP, PAYMENT_METHOD_ID_MERCADOPAGO);
+      console.info(data);
+      setMercadoPagoData(data);
     }
     consultarAPIMP();
   }, [fromDateMP, toDateMP]);
 
+
   // Wow
   useEffect(() => {
     const consultarAPI = async () => {
-      if (fromDateWow !== '' && toDateWow !== '') {
-        guardarIsLoading(true);
-        const validDates = validateTwoDates(fromDateWow,toDateWow);
-
-        if(validDates) {
-          setWowData(dataSet);
-        } else {
-          setWowData([]);
-        }
-
-        // guardarIsLoading(false);
-      } else {
-        setWowData([]);
-      }
+      const data = await getPayments(fromDateWow, toDateWow, PAYMENT_METHOD_ID_WOW);
+      setWowData(data);
     }
     consultarAPI();
   }, [fromDateWow, toDateWow]);
@@ -100,12 +133,8 @@ function Dashboard() {
   // OpenPay
   useEffect(() => {
     const consultarAPI = async () => {
-      if (fromDateOpenPay !== '' && toDateOpenPay !== '') {
-        guardarIsLoading(true);
-
-        setOpenpayData(dataSet);
-        guardarIsLoading(false);
-      }
+      const data = await getPayments(fromDateOpenPay, toDateOpenPay, PAYMENT_METHOD_ID_OPENPAY);
+      setOpenpayData(data);
     }
     consultarAPI();
   }, [fromDateOpenPay, toDateOpenPay]);
@@ -113,18 +142,17 @@ function Dashboard() {
   // Rappi
   useEffect(() => {
     const consultarAPI = async () => {
-      if (fromDateRappi !== '' && toDateRappi !== '') {
-        guardarIsLoading(true);
-
-        setRappiData(dataSet);
-        guardarIsLoading(false);
-      }
+      const data = await getPayments(fromDateRappi, toDateRappi, PAYMENT_METHOD_ID_RAPPI);
+      setRappiData(data);
     }
     consultarAPI();
   }, [fromDateRappi, toDateRappi]);
 
   const validateTwoDates = (first, second) => {
-    if ((new Date(first).getTime() <= new Date(second).getTime())) {
+
+        // let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds() 
+        // console.log(formatted_date)
+    if (first !== null && second !== null && (new Date(first).getTime() <= new Date(second).getTime())) {
       console.info("It's Ok");
       return true;
     }
@@ -144,16 +172,46 @@ function Dashboard() {
               <div className="card-select card-animate card-color-style">
                 <img src={mercadoPago} alt="mercado pago" />
                 <div>
-                  <label>Fecha inicial</label>
-                  <input className="form-control mb-2" type="date"
-                    value={fromDateMP}
-                    onChange={(event) => setFromDateMP(event.target.value)}
-                  />
-                  <label>Fecha final</label>
-                  <input className="form-control mb-2" type="date"
-                    value={toDateMP}
-                    onChange={(event) => setToDateMP(event.target.value)}
-                  />
+
+                  <MuiPickersUtilsProvider locale={esLocale} utils={DateFnsUtils}>
+                    <Grid container justify="space-around">
+                      <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Fecha inicial"
+                        format="MM/dd/yyyy"
+                        value={fromDateMP}
+                        onChange={date => {
+                          console.info(date);
+                          setFromDateMP(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+
+                      <KeyboardDatePicker
+                        margin="normal"
+                        locale="fr"
+                        id="date-picker-dialog"
+                        label="Fecha final"
+                        format="MM/dd/yyyy"
+                        value={toDateMP}
+                        onChange={date => {
+                          console.info(date);
+                          setToDateMP(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+
+                    </Grid>
+                  </MuiPickersUtilsProvider>
 
                   <DownloadExcel dataSet={mercadoPagoData} ></DownloadExcel>
 
@@ -165,16 +223,46 @@ function Dashboard() {
               <div className="card-select card-animate card-color-style">
                 <img src={wow} alt="wow" />
                 <div>
-                  <label>Fecha inicial</label>
-                  <input className="form-control mb-2" type="date"
-                    value={fromDateWow}
-                    onChange={(event) => setFromDateWow(event.target.value)}
-                  />
-                  <label>Fecha final</label>
-                  <input className="form-control mb-2" type="date"
-                    value={toDateWow}
-                    onChange={(event) => setToDateWow(event.target.value)}
-                  />
+
+                  <MuiPickersUtilsProvider locale={esLocale} utils={DateFnsUtils}>
+                    <Grid container justify="space-around">
+                      <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Fecha inicial"
+                        format="MM/dd/yyyy"
+                        value={fromDateWow}
+                        onChange={date => {
+                          console.info(date);
+                          setFromDateWow(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+
+                      <KeyboardDatePicker
+                        margin="normal"
+                        locale="fr"
+                        id="date-picker-dialog"
+                        label="Fecha final"
+                        format="MM/dd/yyyy"
+                        value={toDateWow}
+                        onChange={date => {
+                          console.info(date);
+                          setToDateWow(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+
+                    </Grid>
+                  </MuiPickersUtilsProvider>
 
                   <DownloadExcel dataSet={wowData} ></DownloadExcel>
 
@@ -186,16 +274,45 @@ function Dashboard() {
               <div className="card-select card-animate card-color-style">
                 <img src={openpay} alt="openpay" />
                 <div>
-                  <label>Fecha inicial</label>
-                  <input className="form-control mb-2" type="date"
-                    value={fromDateOpenPay}
-                    onChange={(event) => setFromDateOpenPay(event.target.value)}
-                  />
-                  <label>Fecha final</label>
-                  <input className="form-control mb-2" type="date"
-                    value={toDateOpenPay}
-                    onChange={(event) => setToDateOpenPay(event.target.value)}
-                  />
+
+                  <MuiPickersUtilsProvider locale={esLocale} utils={DateFnsUtils}>
+                    <Grid container justify="space-around">
+                      <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Fecha inicial"
+                        format="MM/dd/yyyy"
+                        value={fromDateOpenPay}
+                        onChange={date => {
+                          console.info(date);
+                          setFromDateOpenPay(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+
+                      <KeyboardDatePicker
+                        margin="normal"
+                        locale="fr"
+                        id="date-picker-dialog"
+                        label="Fecha final"
+                        format="MM/dd/yyyy"
+                        value={toDateOpenPay}
+                        onChange={date => {
+                          console.info(date);
+                          setToDateOpenPay(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+                    </Grid>
+                  </MuiPickersUtilsProvider>
 
                   <DownloadExcel dataSet={openpayData} ></DownloadExcel>
 
@@ -207,16 +324,45 @@ function Dashboard() {
               <div className="card-select card-animate card-color-style">
                 <img src={rappi} alt="rappi" />
                 <div>
-                  <label>Fecha inicial</label>
-                  <input className="form-control mb-2" type="date"
-                    value={fromDateRappi}
-                    onChange={(event) => setFromDateRappi(event.target.value)}
-                  />
-                  <label>Fecha final</label>
-                  <input className="form-control mb-2" type="date"
-                    value={toDateRappi}
-                    onChange={(event) => setToDateRappi(event.target.value)}
-                  />
+
+                  <MuiPickersUtilsProvider locale={esLocale} utils={DateFnsUtils}>
+                    <Grid container justify="space-around">
+                      <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Fecha inicial"
+                        format="MM/dd/yyyy"
+                        value={fromDateRappi}
+                        onChange={date => {
+                          console.info(date);
+                          setFromDateRappi(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+
+                      <KeyboardDatePicker
+                        margin="normal"
+                        locale="fr"
+                        id="date-picker-dialog"
+                        label="Fecha final"
+                        format="MM/dd/yyyy"
+                        value={toDateRappi}
+                        onChange={date => {
+                          console.info(date);
+                          setToDateRappi(date)
+                        }}
+                        KeyboardButtonProps={{
+                          'aria-label': 'change date',
+                        }}
+                        minDate={new Date('2019/11/01')}
+                        maxDate={new Date()}
+                      />
+                    </Grid>
+                  </MuiPickersUtilsProvider>
 
                   <DownloadExcel dataSet={rappiData} ></DownloadExcel>
 
@@ -226,8 +372,7 @@ function Dashboard() {
 
 
           </div>
-          
-          <boxMenu></boxMenu>
+
         </div>
         <div className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-1"></div>
       </div>
